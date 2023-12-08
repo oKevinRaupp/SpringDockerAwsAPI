47b0d228894c01b4c74f4ed8e27a060f918d8c34
oKevinRaupp <rauppke@gmail.com>
2023-12-07 21:40:13 -0300

    first commit

---
+++ src/main/java/com/kevinraupp/api/studydockeraws/StudyDockerAwsApplication.java
@@ -0,0 +1,13 @@
+package com.kevinraupp.api.studydockeraws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDockerAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyDockerAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinraupp/api/studydockeraws/StudyDockerAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinraupp.api.studydockeraws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDockerAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
