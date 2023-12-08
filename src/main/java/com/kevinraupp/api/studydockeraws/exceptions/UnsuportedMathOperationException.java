2b1b3e934dda7d4c0989bd0a48dccadf08c26194
oKevinRaupp <rauppke@gmail.com>
2023-12-08 15:36:06 -0300

    Tratando possíveis exceções.

---
+++ src/main/java/com/kevinraupp/api/studydockeraws/exceptions/UnsuportedMathOperationException.java
@@ -0,0 +1,11 @@
+package com.kevinraupp.api.studydockeraws.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsuportedMathOperationException extends RuntimeException{
+    public UnsuportedMathOperationException(String string){
+        super(string);
+    }
+}
