e51e3aad7be188edd1e25a581c0706017170c8bc
oKevinRaupp <rauppke@gmail.com>
2023-12-09 16:31:51 -0300

    Solving Warning

--- src/main/java/com/kevinraupp/api/studydockeraws/math/SimpleMath.java
+++ src/main/java/com/kevinraupp/api/studydockeraws/math/SimpleMath.java
@@ -17,7 +17,6 @@ public class SimpleMath {
     public Double average (Double num1, Double num2){
         return (num1 + num2) / 2;
     }
-    
     public Double sqrt (Double num1){
         return Math.sqrt(num1);
     }
--- src/main/java/com/kevinraupp/api/studydockeraws/services/PersonServices.java
+++ src/main/java/com/kevinraupp/api/studydockeraws/services/PersonServices.java
@@ -6,9 +6,7 @@ import com.kevinraupp.api.studydockeraws.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
 @Service
 public class PersonServices {
