cd3fe3b235ebfcccf50d53ce959614c9299724a7
oKevinRaupp <rauppke@gmail.com>
2023-12-09 16:28:34 -0300

    Refatorando PersonController

--- src/main/java/com/kevinraupp/api/studydockeraws/controller/PersonController.java
+++ src/main/java/com/kevinraupp/api/studydockeraws/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.kevinraupp.api.studydockeraws.entities.Person;
 import com.kevinraupp.api.studydockeraws.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,8 +35,9 @@ public class PersonController {
         return services.update(person);
     }
     @DeleteMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public void delete(@PathVariable(value = "id") Long id){
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
         services.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
