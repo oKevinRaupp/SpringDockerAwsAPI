f8f5a73d3d1a9c0cd410cc408066bc5218784e4f
oKevinRaupp <rauppke@gmail.com>
2023-12-12 14:10:52 -0300

    Testando os links HATEOAS com mockito e junit 5

---
+++ src/test/java/com/kevinraupp/api/studydockeraws/unittests/mockito/PersonServicesTest.java
@@ -0,0 +1,75 @@
+package com.kevinraupp.api.studydockeraws.unittests.mockito;
+
+import com.kevinraupp.api.studydockeraws.data.model.Person;
+import com.kevinraupp.api.studydockeraws.repositories.PersonRepository;
+import com.kevinraupp.api.studydockeraws.services.PersonServices;
+import com.kevinraupp.api.studydockeraws.unittests.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServicesTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonServices services;
+
+    @Mock
+    PersonRepository repository;
+
+    @BeforeEach
+    void setUpMocks() {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = services.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("Addres Test1",result.getAddress());
+        assertEquals("First Name Test1",result.getFirstName());
+        assertEquals("Last Name Test1",result.getLastName());
+        assertEquals("Female",result.getGender());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void createV2() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
