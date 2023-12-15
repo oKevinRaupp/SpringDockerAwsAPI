e7ac5ba44f6e93e635a130a0cd518377420be789
oKevinRaupp <rauppke@gmail.com>
2023-12-15 00:17:07 -0300

    Metodo para gerar Token!

--- src/main/java/com/kevinraupp/api/studydockeraws/StudyDockerAwsApplication.java
+++ src/main/java/com/kevinraupp/api/studydockeraws/StudyDockerAwsApplication.java
@@ -1,5 +1,6 @@
 package com.kevinraupp.api.studydockeraws;
 
+import com.kevinraupp.api.studydockeraws.security.jwt.GenerateToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
@@ -14,17 +15,6 @@ public class StudyDockerAwsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyDockerAwsApplication.class, args);
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-
-        Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-
-        encoders.put("pbkdf2", pbkdf2Encoder);
-        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
-
-        String result1 = passwordEncoder.encode("admin123");
-        String result2 = passwordEncoder.encode("admin234");
-        System.out.println("My hash result1 " + result1);
-        System.out.println("My hash result2 " + result2);
+        GenerateToken.generateToken();
     }
 }
